f229aa539f2cb0c07185efa38937b27daf37a4cd
SirHall <sirhallstein@gmail.com>
2019-06-27 21:45:35 +1000

    Update definitions and change types to safe cstdint

--- include/volt/defs.hpp
+++ include/volt/defs.hpp
@@ -2,25 +2,33 @@
 #ifndef defs_hpp
 #define defs_hpp
 
+#include <cstdint>
+#include <memory>
 #include <vector>
 
-// This must be equal across the entire program, servers and clients
+// This must be identical across the entire program, servers and clients
 namespace volt
 {
-    constexpr unsigned long buffer_size = 2048;
+    typedef std::uint16_t buffer_size;
+    constexpr buffer_size max_buffer_size = 2048;
     // Must be large enough to contain buffer_size
-    typedef unsigned short  buffer_head;
-    typedef unsigned char   net_word;
-    constexpr unsigned long buffer_mem_size = sizeof(net_word) * buffer_size;
+    typedef std::uint16_t buffer_head;
+    typedef std::uint8_t  net_word; // DO NOT CHANGE
 
-    typedef unsigned short net_tag;
+    constexpr net_word escape_val      = 255;
+    constexpr net_word msg_end_escaped = 0;
+    constexpr net_word msg_origi_char  = 255;
+
+    typedef std::uint16_t net_tag;
 
     // In network messages, the type used to specify the size of an array
-    typedef unsigned int message_array_size;
+    typedef std::uint32_t message_array_size;
 
-    typedef std::vector<net_word>::iterator message_iter;
+    typedef std::vector<net_word>::iterator        message_iter;
+    typedef std::vector<net_word>                  message_t;
+    typedef std::unique_ptr<std::vector<net_word>> message_ptr;
 
-    typedef unsigned char prot_identifier;
+    typedef std::uint8_t prot_identifier;
 } // namespace volt
 
 #endif
