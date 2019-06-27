d0f3ca4ea6159b36a6d201ec79ee01c1a902bafa
SirHall <sirhallstein@gmail.com>
2019-06-27 21:50:41 +1000

    Clean main

--- src/main.cpp
+++ src/main.cpp
@@ -15,40 +15,4 @@ struct test
     char   c = 'c';
 };
 
-int main(int argc, char *argv[])
-{
-    auto array = std::vector({
-        test{1, 1.2, '5'},
-        test{2, 3.141592685, 'g'},
-        test{3, 1.13721839, 'b'},
-        test{4, 1.4, 'y'},
-        test{5, 1.6, 'u'},
-    });
-
-    test t = test();
-
-    auto m = volt::message(0);
-
-    m.write<test>(t);
-
-    m.write<test>(test{123, 4.567, 'a'});
-
-    m.write_array(array);
-
-    m.reset_iterator();
-    test new_t; //= *volt::deserialize::read_new<test>(m.begin() + m.size());
-
-    m.read_into<test>(new_t);
-    std::cout << new_t.a << " " << new_t.b << " " << new_t.c << std::endl;
-    m.read_into<test>(new_t);
-    std::cout << new_t.a << " " << new_t.b << " " << new_t.c << std::endl;
-
-    std::cout << std::endl;
-
-    auto back_array = m.read_new_array<test>();
-
-    for (auto e : *back_array)
-        std::cout << e.a << " " << e.b << " " << e.c << std::endl;
-
-    return 0;
-}
+int main(int argc, char *argv[]) { return 0; }
