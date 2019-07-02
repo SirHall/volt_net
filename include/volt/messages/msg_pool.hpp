7ad470ab8e825dff3f1994878105175a954d68d7
SirHall <sirhallstein@gmail.com>
2019-07-02 18:36:45 +1000

    Refactor msg_pool

--- include/volt/messages/msg_pool.hpp
+++ include/volt/messages/msg_pool.hpp
@@ -8,36 +8,34 @@
 #include <thread>
 #include <vector>
 
-namespace volt
+static std::vector<volt::message_ptr> msg_vec;
+static std::mutex                     _mut;
+static constexpr unsigned int         max_messages = 50;
+
+namespace volt::msg_pool
 {
-    class msg_pool
+
+    volt::message_ptr get_message()
     {
-      private:
-        static std::vector<volt::message_ptr> msg_vec;
-        static std::mutex                     _mut;
-
-      public:
-        static volt::message_ptr get_message()
-        {
-            if (msg_vec.size() > 0)
-            {
-                volt::message_ptr msg;
-                {
-                    std::lock_guard guard(_mut);
-                    msg = std::move(msg_vec.back());
-                    msg_vec.pop_back();
-                }
-
-                return std::move(msg);
-            }
-            return std::move(volt::make_message());
-        }
-        static void return_message(volt::message_ptr msg)
         {
             std::lock_guard guard(_mut);
-            msg_vec.push_back(std::move(msg));
+            if (msg_vec.size() > 0)
+            {
+                volt::message_ptr msg = std::move(msg_vec.back());
+                msg_vec.pop_back();
+                return msg;
+            }
         }
-    };
-} // namespace volt
+        return std::move(volt::make_message());
+    }
+
+    void return_message(volt::message_ptr msg)
+    {
+        std::lock_guard guard(_mut);
+        if (msg_vec.size() >= max_messages)
+            msg_vec.push_back(std::move(msg));
+        // Otherwise, just destroy the message
+    }
+} // namespace volt::msg_pool
 
 #endif
