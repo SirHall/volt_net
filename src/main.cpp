b2d0324d14004b8177a5acfa8908e482d03e7a18
SirHall <sirhallstein@gmail.com>
2019-07-06 16:02:55 +1000

    Updat main to test global events

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
-#include <iostream>
-
-#include "volt/listen/listener.hpp"
-#include "volt/message.hpp"
-#include "volt/serialization.hpp"
-
 #include <chrono>
+#include <iostream>
 #include <string>
 #include <thread>
 
-struct test
+#include "volt/global_events/global_event.hpp"
+#include "volt/global_events/observer.hpp"
+using namespace volt::event;
+
+class test_data
 {
-    int    a = 1;
-    double b = 3.141592658;
-    char   c = 'c';
+  public:
+    test_data() { std::cout << "created" << std::endl; }
+    test_data(test_data &other) = delete;
+    ~test_data() { std::cout << "destroyed" << std::endl; }
+    std::string str = "thing";
 };
 
-int main(int argc, char *argv[]) { return 0; }
+void print_event(test_data &inst) { std::cout << inst.str << std::endl; }
+
+void print_tabbed_event(test_data &inst)
+{
+    std::cout << '\t' << inst.str << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    auto obs       = observer<test_data>(print_event);
+    auto obstabbed = observer<test_data>(print_tabbed_event);
+
+    {
+        auto e      = global_event<test_data>();
+        e.get().str = "Hello there world!";
+    }
+
+    {
+        auto e      = global_event<test_data>();
+        e.get().str = "Hello2";
+    }
+
+    return 0;
+}
