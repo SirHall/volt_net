cd8dc42f0e410f2e39e1a10b07918c6733fb9da8
SirHall <sirhallstein@gmail.com>
2019-07-29 21:14:00 +1000

    Add new endian detector

---
+++ include/volt/volt_endian.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef voltendian_hpp
+#define voltendian_hpp
+
+#include <cstdint>
+
+namespace volt::net
+{
+    // Source: https://stackoverflow.com/a/1001373
+    constexpr bool is_big_endian()
+    {
+        union {
+            std::uint32_t i;
+            char          c[4];
+        } be_test = {0x01020304};
+
+        return be_test.c[0] == 1;
+    }
+} // namespace volt::net
+#endif
