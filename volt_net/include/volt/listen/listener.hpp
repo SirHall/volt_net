b0a51b92d898fc8b75dda502cc84f9f9d19902af
SirHall <sirhallstein@gmail.com>
2019-09-12 12:12:26 +1000

    Documented the listener and moved the listener::accept_new_connection() call to private

--- volt_net/include/volt/listen/listener.hpp
+++ volt_net/include/volt/listen/listener.hpp
@@ -32,15 +32,35 @@ namespace volt
 
         int open_socket();
 
-      public:
-        listener(std::uint16_t hostport);
-
-        ~listener();
-
-        bool is_open();
-
         void accept_new_connection();
 
+      public:
+        /**
+         * @brief Construct a new listener object that listens for new
+         * connections on a desired port.
+         *
+         * @param hostport The port for the listener to find new connections on
+         */
+        listener(std::uint16_t hostport);
+
+        /**
+         * @brief Destroy the listener object
+         *
+         */
+        ~listener();
+
+        /**
+         * @brief Returns the status of the listener
+         *
+         * @return true The listener is currently listening for new connections
+         * @return false The listener is currently offline
+         */
+        bool is_open();
+
+        /**
+         * @brief Closes the listener and sets it to an offline state
+         *
+         */
         void close_listener();
     };
 } // namespace volt
