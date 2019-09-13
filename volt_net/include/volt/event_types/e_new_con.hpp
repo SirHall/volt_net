156627f6f74e01945fcf0b40b42f85479d9fb9c3
SirHall <sirhallstein@gmail.com>
2019-09-13 11:15:32 +1000

    Fully document e_new_con

--- volt_net/include/volt/event_types/e_new_con.hpp
+++ volt_net/include/volt/event_types/e_new_con.hpp
@@ -8,16 +8,41 @@
 
 namespace volt::event
 {
+    /**
+     * @brief The data for a global event that fires when any new connection is
+     * opened
+     *
+     */
     class e_new_con
     {
       private:
         const volt::con_id id;
 
       public:
+        /**
+         * @brief Construct a new e_new_con object with the connection id of the
+         * new connection
+         *
+         * @param new_con_id The connection id of this new connection
+         */
         e_new_con(volt::con_id new_con_id);
+        /**
+         * @brief Forbids object copying
+         *
+         * @param other The instance of the object to copy
+         */
         e_new_con(e_new_con const &other) = delete;
+        /**
+         * @brief Destroy the e_new_con object
+         *
+         */
         ~e_new_con();
 
+        /**
+         * @brief Returns the connection id of the new connection
+         *
+         * @return volt::con_id The connection id of the new connection
+         */
         volt::con_id get_id() const;
     };
 } // namespace volt::event
