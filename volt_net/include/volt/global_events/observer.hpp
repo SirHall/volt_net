388cbed08ddff0e5fd7189cc9a063527da591cb0
SirHall <sirhallstein@gmail.com>
2019-09-13 11:16:14 +1000

    Fully document the observer

--- volt_net/include/volt/global_events/observer.hpp
+++ volt_net/include/volt/global_events/observer.hpp
@@ -8,6 +8,13 @@
 
 namespace volt::event
 {
+    /**
+     * @brief The observer is the class that handles subscriptions to the
+     * global_event template class of the same type. Any function handed to it
+     * will be fired when the global_event callback is too.
+     *
+     * @tparam T The type of the callback to subscribe to
+     */
     template <typename T>
     class observer
     {
@@ -15,13 +22,29 @@ namespace volt::event
         std::function<void(T const &)> event;
 
       public:
+        /**
+         * @brief Forbids observer copying
+         *
+         * @param other The instance of the other observer to copy
+         */
         observer(observer const &other) = delete; // No copying allowed
+        /**
+         * @brief Construct a new observer object given a function and subscribe
+         * to the corresponding global_event class template
+         *
+         * @param action The function to invoke when the event is fired
+         */
         observer(std::function<void(T const &)> action)
         {
             // Subscribe to global_event
             event = action;
             global_event<T>::subscribe(&event);
         }
+        /**
+         * @brief Destroy the observer object and unsubscribe from the
+         * corresponding global_event class template
+         *
+         */
         ~observer()
         {
             std::cout << "Before del" << std::endl;
