405dae85f5c8c740d3db1fb2b1f995399c8b2e99
SirHall <sirhallstein@gmail.com>
2019-09-13 11:15:58 +1000

    Fully document global_event

--- volt_net/include/volt/global_events/global_event.hpp
+++ volt_net/include/volt/global_events/global_event.hpp
@@ -9,22 +9,52 @@
 
 namespace volt::event
 {
+    /**
+     * @brief A templated class that allows for global events. Useful for pure
+     * decoupling of many systems
+     *
+     * @tparam T The type of that data that will be handed to all observers
+     */
     template <typename T>
     class global_event
     {
       protected:
+        /**
+         * @brief A list of all subscribed functions that are to be invoked when
+         * this event is fired
+         *
+         */
         static inline std::vector<std::function<void(T const &)> *> subscribers;
 
       public:
-        global_event()  = delete;
+        /**
+         * @brief Forbids public construction of the global_event class
+         *
+         */
+        global_event() = delete;
+        /**
+         * @brief Forbids public destruction of the global_event classs
+         *
+         */
         ~global_event() = delete;
 
+        /**
+         * @brief Invokes all registered observers
+         *
+         * @param inst An instance of the data of type T to share between all
+         * observers
+         */
         static void call_event(T const &inst)
         {
             for (auto subscriber : subscribers)
                 (*subscriber)(inst);
         }
 
+        /**
+         * @brief Will subscribe an observer to this event
+         *
+         * @param obs The observer that will get called
+         */
         static void subscribe(std::function<void(T const &)> *obs)
         {
             std::cout << "Subscribed" << std::endl;
@@ -32,6 +62,11 @@ namespace volt::event
             std::cout << subscribers.size() << std::endl;
         }
 
+        /**
+         * @brief Removes a registered observer from this event
+         *
+         * @param obs The observer to remove from the callback list
+         */
         static void unsubscribe(std::function<void(T const &)> *obs)
         {
             std::cout << "Unsubbing" << std::endl;
