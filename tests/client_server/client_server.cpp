746dd137e152961601cb2ceba49aa4e78fb46514
SirHall <sirhallstein@gmail.com>
2019-12-10 16:49:40 +1000

    Update client_server test to use new API

--- tests/client_server/client_server.cpp
+++ tests/client_server/client_server.cpp
@@ -4,9 +4,11 @@
 #include "volt/listen/listener.hpp"
 #include "volt/messages/msg_reader.hpp"
 #include "volt/messages/msg_rec_pool.hpp"
+#include "volt/net/network.hpp"
 #include "volt/net_con.hpp"
 #include "volt/serialization/net_serialize.hpp"
 #include "volt/serialization/serialization_ext.hpp"
+#include "volt/volt_defs.hpp"
 
 #include <atomic>
 #include <chrono>
@@ -14,16 +16,12 @@
 #include <string>
 #include <thread>
 
-using namespace volt;
-using namespace volt::serialize;
-using namespace volt::deserialize;
-
 #pragma region Network Test Data
 
 struct test
 {
-    int a = 1;
-    // double      b = 3.141592658;
+    int         a = 1;
+    double      b = 3.141592658;
     char        c = 'c';
     std::string s = "!A basic test string!";
     bool        operator==(const struct test &rhs)
@@ -35,31 +33,24 @@ struct test
 // This is the object we will send over the network
 static struct test send_val = test();
 
-namespace volt::serialize
+template <>
+void volt::net::serialize::write_into(struct test const &v, message_ptr &data)
 {
-    template <>
-    void volt::serialize::write_into(struct test const &v,
-                                     volt::message_ptr &data)
-    {
-        write_into(v.a, data);
-        // write_into(v.b, data);
-        write_into(v.c, data);
-        write_into(v.s, data);
-    }
-} // namespace volt::serialize
+    volt::net::serialize::write_into(v.a, data);
+    // volt::net::serialize::write_into(v.b, data);
+    volt::net::serialize::write_into(v.c, data);
+    volt::net::serialize::write_into(v.s, data);
+}
 
-namespace volt::deserialize
+template <>
+void volt::net::deserialize::read_into(message_iter &iterator,
+                                       struct test & instance)
 {
-    template <>
-    void volt::deserialize::read_into(message_iter &iterator,
-                                      struct test & instance)
-    {
-        read_into(iterator, instance.a);
-        // read_into(iterator, instance.b);
-        read_into(iterator, instance.c);
-        read_into(iterator, instance.s);
-    }
-} // namespace volt::deserialize
+    volt::net::deserialize::read_into(iterator, instance.a);
+    // volt::net::deserialize::read_into(iterator, instance.b);
+    volt::net::deserialize::read_into(iterator, instance.c);
+    volt::net::deserialize::read_into(iterator, instance.s);
+}
 
 #pragma endregion
 
@@ -74,7 +65,7 @@ void handle_close(int s)
     error_quit = true;
 }
 
-void msg_event(volt::reader_ptr const &reader)
+void msg_event(volt::net::reader_ptr const &reader)
 {
     struct test rec_val; // Recieve this from the network
     reader->read_msg(rec_val);
@@ -91,44 +82,79 @@ int main(int argc, char *argv[])
 {
     signal(SIGINT, handle_close); // User pressed Ctrl + C
 
-    auto obs = volt::event::observer<volt::reader_ptr>(msg_event);
+    // Setup send_val
+    send_val.a = -104;
+    // send_val.b = 1023.2134;
+    send_val.c = 'p';
+    send_val.s = "Msg sent correctly";
+
+    auto network = volt::net::network::create(8, true);
+    network->set_new_msg_callback([](volt::net::reader_ptr reader) {
+        test t = test();
+        reader->read_msg(t);
+        std::cout << "a: " << t.a << std::endl;
+        // std::cout << "b: " << t.b << std::endl;
+        std::cout << "c: " << t.c << std::endl;
+        std::cout << "s: " << t.s << std::endl;
+        std::cout << std::endl;
+    });
 
     std::cout << "Server launched" << std::endl;
 
-    // Instantiate 10 clients
-
     {
-        auto net_listener = volt::listener(64420);
-        if (!net_listener.is_open())
+        auto net_lock = network->get_guard();
+        network->start_listening(64430, 64431, net_lock);
+    }
+
+    // Instantiate 10 clients
+    for (int i = 0; (i < 10) && !error_quit; i++)
+    {
+        auto con_status = network->server_connect("127.0.0.1", "64430");
+        int  dot_count  = 1;
+        while (con_status->GetState() ==
+                   volt::net::ServerConnectStatus::Connecting &&
+               !error_quit)
         {
-            std::cerr << "Failed to open listener" << std::endl;
-            return 1;
+            // Wait until something actually happens
+            // std::cout << "\rConnecting"; //<< std::endl;
+            // for (int i = 1; i <= 3; i++)
+            //     std::cout << ((i <= dot_count) ? "." : " ");
+            // std::cout << std::flush;
+            dot_count = (dot_count % 3) + 1;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
 
-        for (int i = 0; i < 10; i++)
-            volt::net_con::server_connect("localhost", "64420");
+        if (con_status->GetState() == volt::net::ServerConnectStatus::Connected)
+        {
+            std::cout << "Client connected" << std::endl;
+        }
+        else if (con_status->GetState() ==
+                 volt::net::ServerConnectStatus::Failed)
+        {
+            std::cout << "Connection attempt failed" << std::endl;
+        }
+    }
 
-        while (true)
+    // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    {
+
+        for (std::size_t send_i = 0; (send_i < 10) && !error_quit; send_i++)
         {
             // Send data on connections
-            auto writer = volt::msg_writer();
+            auto writer = volt::net::msg_writer();
 
             writer.write_msg(send_val);
 
             {
-                auto lock = volt::net_con::aquire_lock();
+                auto net_lock = network->get_guard();
 
-                for (unsigned int i = 0; i < volt::net_con::con_count(lock);
+                for (unsigned int i = 0; i < network->get_con_count(net_lock);
                      i++)
                 {
-                    if (auto &con = volt::net_con::get_con(i, lock))
+                    if (auto &con = network->get_con_index(i, net_lock))
                     {
-                        if (!con->send_msg(writer.get_msg()))
-                        {
-                            std::cerr << "Failed to send data over connection"
-                                      << std::endl;
-                            return 1;
-                        }
+                        con->send_msg(writer.get_msg());
                     }
                     else
                     {
@@ -139,25 +165,37 @@ int main(int argc, char *argv[])
                 }
             }
 
-            {
-                auto lock = std::lock_guard(cnt_mut);
-                if (msgs_recieved >= 100)
-                {
-                    std::cout << "Test succeeded, all data transfered correctly"
-                              << std::endl;
-                    break;
-                }
-            }
-            if (error_quit)
-            {
-                std::cerr << "Error occurred, forcefully closing the server"
-                          << std::endl;
-                return 1;
-            }
+            // {
+            //     auto lock = std::lock_guard(cnt_mut);
+            //     if (msgs_recieved >= 100)
+            //     {
+            //         std::cout << "Test succeeded, all data transfered
+            //         correctly"
+            //                   << std::endl;
+            //         break;
+            //     }
+            //     if (error_quit)
+            //         break;
+            // }
+            // if (error_quit)
+            // {
+            //     std::cerr << "Error occurred, forcefully closing the server"
+            //               << std::endl;
+            //     return 1;
+            // }
 
-            // std::this_thread::sleep_for(std::chrono::milliseconds(500));
-            volt::net::notify_listeners();
+            network->process_new_msgs();
         }
     }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    network->process_new_msgs();
+
+    error_quit = true; // Just incase
+    {
+        auto net_lock = network->get_guard();
+        network->close(net_lock);
+    }
+    std::cout << "End of main()" << std::endl;
     return 0;
 }
