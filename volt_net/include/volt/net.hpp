fec41f931f29ccc8f99872d9cccea475430ac32e
SirHall <sirhallstein@gmail.com>
2019-12-11 19:14:46 +1000

    Added volt/net.hpp header to include all library headers

---
+++ volt_net/include/volt/net.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef net_hpp
+#define net_hpp
+
+#include "volt/net/AsyncStateHandler.hpp"
+#include "volt/net/listener.hpp"
+#include "volt/net/messages/msg_pool.hpp"
+#include "volt/net/messages/msg_reader.hpp"
+#include "volt/net/messages/msg_rec_pool.hpp"
+#include "volt/net/messages/msg_writer.hpp"
+#include "volt/net/net_con.hpp"
+#include "volt/net/network.hpp"
+#include "volt/net/serialization/net_serialize.hpp"
+#include "volt/net/serialization/serialization_ext.hpp"
+#include "volt/net/volt_endian.hpp"
+#include "volt/net/volt_net_defs.hpp"
+
+#endif
