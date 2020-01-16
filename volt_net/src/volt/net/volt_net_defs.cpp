bdf5488f33772f20e593b0b5f94a8f9f5f29a200
SirHall <sirhallstein@gmail.com>
2020-01-16 16:00:53 +1000

    Make message_ptr return the message to msg_pool only when message_ptr has finished with it

--- volt_net/src/volt/net/volt_net_defs.cpp
+++ volt_net/src/volt/net/volt_net_defs.cpp
@@ -1,8 +1,15 @@
 #include "volt/net/volt_net_defs.hpp"
 
+#include "volt/net/messages/msg_pool.hpp"
+
 using namespace volt::net;
 
 message_ptr volt::net::make_message()
 {
-    return std::make_shared<std::vector<net_word>>();
+    // When destroyed, the raw message pointer will be given to msg_pool, which
+    // will then decide what will finally happen with the message. Whether it
+    // should be pooled, or deleted.
+    return std::shared_ptr<std::vector<net_word>>(
+        new std::vector<net_word>(),
+        [=](std::vector<net_word> *msg) { msg_pool::return_message(msg); });
 }
